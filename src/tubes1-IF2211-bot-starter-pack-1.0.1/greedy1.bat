471b5618d4ba53ae35034e4986b908020efbef10
BiZaRrE96 <91012945+BiZaRrE96@users.noreply.github.com>
2024-03-09 22:31:51 +0700

    quick name change

--- src/tubes1-IF2211-bot-starter-pack-1.0.1/greedy1.bat
+++ src/tubes1-IF2211-bot-starter-pack-1.0.1/greedy1.bat
@@ -1,6 +1,6 @@
 @echo off
-start cmd /c "python main.py --logic GreedyQais --email=greedyqais@email.com --name=greedyqais --password=123456 --team etimo"
-start cmd /c "python main.py --logic GreedyJustin --email=greedymuneki@email.com --name=greedymnki --password=123456 --team etimo"
-start cmd /c "python main.py --logic GreedyDave --email=greedydave@email.com --name=greedydave --password=123456 --team etimo"
+start cmd /c "python main.py --logic GreedyQais --email=greedyqais@email.com --name=qais --password=123456 --team etimo"
+start cmd /c "python main.py --logic GreedyJustin --email=greedymuneki@email.com --name=mnki --password=123456 --team etimo"
+start cmd /c "python main.py --logic GreedyDave --email=greedydave@email.com --name=dave --password=123456 --team etimo"
 
 @REM the two other commented greedy commands is just for commermorating other greedy algorithms that we made
